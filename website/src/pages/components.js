af874aef816a04358350e6620eb6cf8a24aeacaa
Mark Kvetny <biowaffeln@gmail.com>
2019-09-24 22:20:42 +0100

    add sidebar

--- website/src/pages/components.js
+++ website/src/pages/components.js
@@ -1,7 +1,16 @@
 import React from "react";
-import { ThemeProvider, createGlobalStyle } from "styled-components";
+import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
 import theme from "../system/theme";
-import { Box, Heading, Button, Text, Img, themeGet } from "../system/base";
+import {
+  Box,
+  Heading,
+  Button,
+  Text,
+  Img,
+  themeGet,
+  Flex,
+  Link,
+} from "../system/base";
 import LiveExample from "./components/LiveExample";
 
 const Global = createGlobalStyle`
@@ -86,24 +95,87 @@ const data = [
   },
 ];
 
-const Components = () => (
-  <ThemeProvider theme={theme}>
-    <Box maxWidth="2xl" mx="auto">
-      <Global />
-      <Heading fontSize="5xl">Component Playground</Heading>
-      <Text fontSize="xl">
-        This is a playground where you can see examples of the different base
-        components provided with styled-system-tailwind.
-      </Text>
-      {data.map(({ name, scope, code }) => (
-        <React.Fragment key={name}>
-          <Heading as="h2" id={name} color="gray.800" fontSize="3xl" mb="4">
-            {name} Component
-          </Heading>
-          <LiveExample scope={scope} code={code} mb="10" />
-        </React.Fragment>
+const ScrollLink = props => {
+  const smoothScroll = e => {
+    e.preventDefault();
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const offsetTop =
+      document.getElementById(id).getBoundingClientRect().top +
+      window.pageYOffset;
+    window.scroll({
+      top: offsetTop,
+      behavior: "smooth",
+    });
+  };
+  return <Link display="block" {...props} onClick={smoothScroll} />;
+};
+
+const StyledScrollLink = styled(ScrollLink)`
+  &:hover {
+    color: black;
+  }
+`;
+
+const Sidebar = props => (
+  <Box display={["none", "none", "block"]} width={48} {...props}>
+    <Box px="5" mt="12" position="fixed">
+      {data.map(({ name }) => (
+        <StyledScrollLink
+          mb="2"
+          fontWeight="bold"
+          color="gray.700"
+          fontSize="xl"
+          textDecoration="none"
+          px="2"
+          borderRadius="normal"
+          href={`#${name}`}
+        >
+          {name}
+        </StyledScrollLink>
       ))}
     </Box>
+  </Box>
+);
+
+const Content = props => (
+  <Box
+    mx={[5, 10]}
+    my="10"
+    bg="white"
+    maxWidth="3xl"
+    borderRadius="normal"
+    {...props}
+  >
+    <Box px={[6, 8, 12]}>
+      <Box py="10">
+        <Heading lineHeight="none" mb="6" fontSize="5xl">
+          Component Playground
+        </Heading>
+        <Text fontSize="xl">
+          This is a playground where you can see examples of the different base
+          components provided with styled-system-tailwind.
+        </Text>
+      </Box>
+      {data.map(({ name, scope, code }) => (
+        <Box key={name}>
+          <Heading as="h2" id={name} color="gray.800" fontSize="3xl" mb="6">
+            {name} Component
+          </Heading>
+          <LiveExample scope={scope} code={code} mb="16" />
+          {/* <Box as="hr" mb="12" mx={-10} borderColor="gray.400" /> */}
+        </Box>
+      ))}
+    </Box>
+  </Box>
+);
+
+const Components = () => (
+  <ThemeProvider theme={theme}>
+    <Flex>
+      <Global />
+      <Sidebar />
+      <Content />
+    </Flex>
   </ThemeProvider>
 );
 
