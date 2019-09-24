9841943d4f80a14913bbf54826226184d9ea63a5
Mark Kvetny <biowaffeln@gmail.com>
2019-09-24 22:20:33 +0100

    change code styles

--- website/src/pages/components/LiveExample.js
+++ website/src/pages/components/LiveExample.js
@@ -1,14 +1,22 @@
 import React from "react";
-import { LiveProvider, LivePreview, LiveEditor, LiveError } from "react-live";
+import { LiveProvider, LivePreview, LiveEditor } from "react-live";
 import { Box } from "../../system/base";
+import { codeTheme } from "./codeTheme";
 
 const LiveExample = ({ scope, code, ...rest }) => (
-  <Box boxShadow="md" borderRadius="lg" overflow="hidden" {...rest}>
-    <LiveProvider scope={scope} code={code}>
-      <Box p="5" bg="white">
+  <Box {...rest}>
+    <LiveProvider scope={scope} code={code} theme={codeTheme}>
+      <Box
+        borderRadius="lg"
+        borderWidth="1"
+        overflow="hidden"
+        p="5"
+        mb="4"
+        bg="gray.100"
+      >
         <LivePreview />
       </Box>
-      <Box bg="gray.900">
+      <Box borderRadius="lg" overflow="hidden" bg="gray.900">
         <LiveEditor style={{ caretColor: "white" }} />
       </Box>
     </LiveProvider>
